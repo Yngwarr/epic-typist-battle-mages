e0dcd3c886a556a98431868818bbfdae1cb18088
Yngvarr <k.yngvarr@gmail.com>
2024-06-26 14:47:26 +0200

    added HUD to test sockets

--- frontend/src/SocketClient.gd
+++ frontend/src/SocketClient.gd
@@ -1,16 +1,20 @@
 extends Node
 
+signal message_received(message: String)
+
 var socket = WebSocketPeer.new()
 
 func _ready() -> void:
-	socket.connect_to_url("wss://example.com")
+	socket.connect_to_url("ws://localhost:9092")
 
 func _process(_delta: float) -> void:
 	socket.poll()
 	var state := socket.get_ready_state()
 	if state == WebSocketPeer.STATE_OPEN:
 		while socket.get_available_packet_count():
-			print("Packet: ", socket.get_packet())
+			var packet := socket.get_packet()
+			print("Packet: ", packet)
+			message_received.emit(packet.get_string_from_utf8())
 	elif state == WebSocketPeer.STATE_CLOSING:
 		# Keep polling to achieve proper close.
 		pass
@@ -19,3 +23,6 @@ func _process(_delta: float) -> void:
 		var reason = socket.get_close_reason()
 		print("WebSocket closed with code: %d, reason %s. Clean: %s" % [code, reason, code != -1])
 		set_process(false) # Stop processing.
+
+func send(message: String) -> void:
+	socket.send_text(message)
