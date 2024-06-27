d6dbb1704a2167435ab33110bc1f557a0c7bd3bd
spicyfalafel <285574@niuitmo.ru>
2024-06-27 23:35:07 +0300

    fix wrong debuff target

--- backend/src/main/java/org/example/entity/spell/debuff/DebuffSpell.java
+++ backend/src/main/java/org/example/entity/spell/debuff/DebuffSpell.java
@@ -9,7 +9,7 @@ public abstract class DebuffSpell implements Spell {
 
     @Override
     public void processSpell(List<Player> all, Player from, Object... args) {
-        debuffPlayer(from);
+        debuffPlayer((Player) args[0]);
     }
 
     abstract void debuffPlayer(Player player);
