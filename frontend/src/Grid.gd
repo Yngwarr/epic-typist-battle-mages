2ebc34d586a77d1ccf5a8138b64c6ee20e9a8088
Yngvarr <k.yngvarr@gmail.com>
2024-06-27 22:45:44 +0200

    image of a wizard + fix of a white trail

--- frontend/src/Grid.gd
+++ frontend/src/Grid.gd
@@ -1,10 +1,5 @@
 extends TileMap
 
-func _ready() -> void:
-	for child in get_children():
-		set_cell(0, local_to_map(child.position), child.type, Vector2i.ZERO)
-
-
 func get_cell_pawn(cell: Vector2i, type: Global.CellType = Global.CellType.ACTOR) -> Node2D:
 	for node in get_children():
 		if node.type != type:
@@ -27,8 +22,8 @@ func request_move(pawn: Pawn, direction: Vector2i) -> Vector2i:
 	var cell_tile_id := get_cell_source_id(0, cell_target)
 
 	if cell_tile_id == -1:
-		set_cell(0, cell_target, Global.CellType.ACTOR, Vector2i.ZERO)
-		set_cell(0, cell_start, -1, Vector2i.ZERO)
+		# set_cell(0, cell_target, Global.CellType.ACTOR, Vector2i.ZERO)
+		# set_cell(0, cell_start, -1, Vector2i.ZERO)
 		return map_to_local(cell_target)
 
 	return Vector2i.ZERO
--- frontend/src/mage.gd
+++ frontend/src/mage.gd
@@ -1,2 +1,6 @@
 extends Node2D
 
+@onready var anim: AnimationPlayer = $AnimationPlayer
+
+func _ready() -> void:
+	anim.play(&"idle")
