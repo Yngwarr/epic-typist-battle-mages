07dceb21bb820888365ec4a3545cb0652052dd20
Gorovoi <derexte@gmail.com>
2024-06-28 02:55:14 +0300

    fronted: add fireballs and explosion

--- frontend/src/SpellListPanel.gd
+++ frontend/src/SpellListPanel.gd
@@ -4,12 +4,15 @@ extends Panel
 @onready var slow_debuff := $HBoxContainer/Stickiness
 @onready var magic_missile := $HBoxContainer/MagicMissile
 @onready var confusion := $HBoxContainer/Confusion
-
+@onready var fireball := $HBoxContainer/Fireball
+@onready var explosion := $HBoxContainer/Explosion
 
 @onready var spells : Array[Spell] = [
 	magic_missile,
 	slow_debuff,
-	confusion
+	confusion,
+	fireball,
+	explosion
 ]
 
 func get_spell(index: int) -> Spell:
